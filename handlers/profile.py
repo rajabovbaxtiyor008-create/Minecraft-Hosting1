2e7514d136e282f76d46ab544e5767edb9a0e50c
rajabovbaxtiyor008-create <rajabovbaxtiyor008@gmail.com>
2026-07-24 22:54:31 +0500

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -1,1 +1,34 @@
+from aiogram import Router, F
+from aiogram.types import Message
+import sqlite3
 
+router = Router()
+
+
+@router.message(F.text == "👤 Профиль")
+async def profile(message: Message):
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT balance FROM users WHERE telegram_id=?",
+        (message.from_user.id,)
+    )
+
+    user = cur.fetchone()
+
+    if user:
+        balance = user[0]
+    else:
+        balance = 0
+
+    conn.close()
+
+    await message.answer(
+        f"""👤 <b>Ваш профиль</b>
+
+🆔 ID: <code>{message.from_user.id}</code>
+💰 Баланс: {balance} ₽
+📦 Покупок: скоро будет
+"""
+    )
