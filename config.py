cd8975731a69576251d15ce33092c98304812532
rajabovbaxtiyor008-create <rajabovbaxtiyor008@gmail.com>
2026-07-24 22:40:05 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# config.py
+
+BOT_TOKEN = "8920386800:AAEMG39dLXKi9W2qjLo0uiljRqYO2v5tNbg"
+
+ADMIN_IDS = [
+    123456789  # Замени на свой Telegram ID
+]
+
+DATABASE = "database.db"
+
+SHOP_NAME = "Minecraft Build Shop"
+
+SUPPORT = "@your_support"
+
+CURRENCY = "₽"
+
+PAYMENT_TOKEN = ""  # Если подключишь оплату
+
+CHANNEL = ""  # Канал для подписки (необязательно)
